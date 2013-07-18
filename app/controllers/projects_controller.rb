47fb3d4d7b01d8073f387cb4f9fd4323e44a63a7
Stephanie Datu <stephdatu@gmail.com>
2013-07-18 15:57:30 -0700

    Add admin namespaced users controller

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -44,15 +44,7 @@ class ProjectsController < ApplicationController
 	end
 
 	private
-		def authorize_admin!
-			authenticate_user!
-			unless current_user.admin?
-				flash[:alert] = "You must be an admin to do that."
-				redirect_to root_path
-			end
-		end
-
-		def find_project
+			def find_project
 			@project = Project.find(params[:id])
 			rescue ActiveRecord::RecordNotFound
 			flash[:alert] = "The project you were looking for could not be found."
